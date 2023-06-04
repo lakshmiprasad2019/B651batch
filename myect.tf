e33dc76a9e3e9bd90a6ff51b2e4ad633026c162a
lakshmiprasad2019 <58114347+lakshmiprasad2019@users.noreply.github.com>
2023-06-04 12:02:18 +0530

    Update myect.tf

--- myect.tf
+++ myect.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "foo" {
   ami           = "ami-0715c1897453cabd1" # us-west-2
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
   key_name      = "kalyan_nvirginia"
 }
